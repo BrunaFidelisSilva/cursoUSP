6d7bf20ac74b279ab6935f58156508a7dbb9b96b
BrunaFidelisSilva <126256920+BrunaFidelisSilva@users.noreply.github.com>
2023-02-24 10:05:11 -0300

    Add files via upload

---
+++ Semana1.py
@@ -0,0 +1,41 @@
+x = input()
+valor = (x=='True')
+print(valor)
+print(not valor)
+
+"""
+#Seu programa deve ler um inteiro, calcular o próximo valor inteiro, e imprimir o valor calculado.
+from tkinter import Y
+
+
+print('Digite um numero para ver seu sucessor:')
+x = int(input())
+x = x+1
+print(x)
+
+#Seu programa deve ser um inteiro, calcular o valor inteiro anterior, e imprimir o valor calculado.
+print('Digite um numero para ver seu anterior:')
+x = int(input())
+x = x-1
+print(x)
+
+#Seu programa deve ser um inteiro, calcular o dobro do valor lido, e imprimir o valor calculado.
+print('Digite um numero para ver seu dobro:')
+x = int(input())
+x = x*2
+print(x)
+
+#Seu programa deve ser um inteiro, calcular a metade do valor lido, e imprimir o valor calculado.
+print('Digite um numero para ver sua metade:')
+x = int(input())
+x = x/2
+print(x)
+
+Seu programa deve ler dois valores inteiros, um em cada linha, e calcular o resultado da divisão inteira do primeiro pelo segundo e 
+o resultado do resto da divisão inteira do primeiro pelo segundo e imprimir os valores calculados.
+x = int(input())
+y = int(input())
+a = int(x/y)
+b = int(x-a*y)
+print(a)
+print(b)"""
